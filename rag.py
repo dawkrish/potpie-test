73ac171d2e3ad141fa57a023da88ca71b55e65ff
dawkrish <krishnansh710@gmail.com>
2024-11-08 14:01:52 +0530

    search_kwargs

--- rag.py
+++ rag.py
@@ -1,33 +1,56 @@
 import os
 
-import chromadb.utils.embedding_functions
 from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
 from langchain_mistralai import ChatMistralAI
-from langchain.agents import initialize_agent, Tool
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+from langchain import hub
+from langchain_core.runnables import RunnablePassthrough
 from langchain_chroma import Chroma
 
 from langchain_core.output_parsers import StrOutputParser
 
-from embeddings import get_default_embeddings
+from embeddings import chroma_client
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["ANTHROPIC_API_KEY"] = os.getenv("ANTHROPIC_API_KEY")
 os.environ["MISTRAL_API_KEY"] = os.getenv("MISTRAL_API_KEY")
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 # model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
 
-model = ChatMistralAI(model="mistral-large-latest")
-
+llm = ChatMistralAI(model="mistral-large-latest")
 parser = StrOutputParser()
-chain = model | parser
+prompt = hub.pull("rlm/rag-prompt")
 
-vectorstore = Chroma(persist_directory="./db",
-                     embedding_function=chromadb.utils.embedding_functions.DefaultEmbeddingFunction())
-retriever = vectorstore.as_retriever()
+hugging_face_embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+vectorstore = Chroma(client=chroma_client,
+                     embedding_function=hugging_face_embeddings,
+                     collection_name="my_embeddings"
+                     )
 
+# data = vectorstore.similarity_search(query="Explain this doc", filter={"asset_id" : "70c81f8e-b2a2-4e54-9a34-c71c6d206d72"})
+# print(data)
 
-def respond_to_message(user_message, embs):
-    for i in chain.stream():
-        retriever.g
-        print(i)
+def respond_to_message(user_message, asset_id):
+    print(asset_id)
+    retriever = vectorstore.as_retriever(
+        search_kwargs={
+            'filter' : {'asset_id' : asset_id}
+            }
+        )
+    print(retriever)
+
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | parser
+    )
+    results = rag_chain.invoke(user_message)
+    return results
